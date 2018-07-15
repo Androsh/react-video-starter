075c381dbb1cd39e50b3a0d08b64e9a6b8f521be
androsh <apanasiuk00@gmail.com>
2018-07-15 23:09:09 +0100

    Create video lists and connect to main app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 const API_KEY = "AIzaSyBm7GrJJ69DKSxya0tsTztAEvt7Xso1hsE";
 
 // Create a new component. This components should produce some HTML
@@ -11,8 +12,9 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: "games" }, data => {
-      this.setState({ videos: data });
+    YTSearch({ key: API_KEY, term: "games" }, videos => {
+      this.setState({ videos });
+      // this.setState({ videos: videos});
     });
   }
 
@@ -20,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
