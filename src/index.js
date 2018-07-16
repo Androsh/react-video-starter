d26d4427bc7c6213e14b591021fd22809966912f
Andrzej Panasiuk <apanasiuk00@gmail.com>
2018-07-16 15:58:19 +0100

    Search bar functionality

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component {
       selectedVideos: null
     };
 
-    YTSearch({ key: API_KEY, term: "fcuk" }, videos => {
+    this.videoSearch("fcuk");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
